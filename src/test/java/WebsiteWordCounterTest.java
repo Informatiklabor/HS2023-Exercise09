342c82f5c80ddf5f17c33c1c7b7dedde6072ca1a
Ralph Gasser <ralph.gasser@unibas.ch>
2023-11-20 11:54:56 +0100

    Fixes a typo.

--- src/test/java/WebsiteWordCounterTest.java
+++ src/test/java/WebsiteWordCounterTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class WebsiteWordCounterTest {
     
     /**
-     * This method should test the WebsiteWordCouner.test() method.
+     * This method should test the WebsiteWordCounter.count() method.
      * 
      * Please implement it.
      */
